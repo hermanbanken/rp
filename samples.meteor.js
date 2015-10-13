061c2a2d108c70c64879f38bc257aa61f6d90422
Herman Banken <hermanbanken@gmail.com>
2015-10-13 11:12:59 +0200

    Signal & Meteor.Deps

--- samples.meteor.js
+++ samples.meteor.js
@@ -1,13 +1,39 @@
 //// signal-compose ////
 
+Template.HUD.tempF = Temperature;
+Template.HUD.tempC = () => Temperature() * 1.8 + 32;
+
 <template name="HUD">
   <div>Celcius: {{tempC}}</div>
   <div>Fahrenheit: {{tempF}}</div>
 </template>
 
-Template.HUD.tempF = Temperature;
-Template.HUD.tempC = function(){
-  return Temperature() * 1.8 + 32;
-}
+////////////////////////
+
+//// deps ////
+
+var favoritePie = "apple";
+var favoritePieDep = new Deps.Dependency;
+
+var getFavoritePie = function () {
+  favoritePieDep.depend();
+  return favoritePie;
+};
+
+var setFavoritePie = function (newValue) {
+  favoritePie = newValue;
+  favoritePieDep.changed();
+};
+
+getFavoritePie();
+// "apple"
+
+var handle = Deps.autorun(function () {
+  console.log("Your favorite Pie is " + getFavoritePie());
+});
+// "Your favorite Pie is apple"
+
+setFavoritePie("chocolate");
+// "Your favorite Pie is chocolate"
 
 ////////////////////////
--- samples.scala
+++ samples.scala
@@ -2,6 +2,7 @@
 
 val tempC = Signal{ Temperature() }
 val tempF = Signal{ tempC() * 1.8 + 32}
+
 observe(tempC) { C => /* print on label */ }
 observe(tempF) { F => /* print on label */ }
 
