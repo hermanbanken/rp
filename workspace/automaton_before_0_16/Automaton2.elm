534561894726304f9fe1d69b4f81931e7a4f25e5
Herman Banken <hermanbanken@gmail.com>
2017-06-07 16:30:28 +0200

    Add old Elm automaton sample from Evan

---
+++ workspace/automaton_before_0_16/Automaton2.elm
@@ -0,0 +1,38 @@
+-- Source: https://groups.google.com/forum/#!searchin/elm-discuss/automaton%7Csort:relevance/elm-discuss/9R-sld6eQ78/FQ4XWqZOujYJ
+import Automaton
+import Signal.Input
+import Signal.Random
+import Data.List
+
+(butnMore,pressMore) = button "  +  "
+(butnLess,pressLess) = button "  -  "
+
+data Command = Incr | Decr | Idnt
+
+formsAutomaton =
+  let fstep (cmd,pos,color,mouse) fs =
+          let fs' = case cmd of
+                    { Incr -> fs ++ [dragForm $ filled color (rect 40 40 pos)]
+                    ; Decr -> if fs == [] then [] else tail fs
+                    ; Idnt -> fs }
+          in  unzip $ map (step mouse) fs'
+  in  init' [] fstep
+
+allInput =
+  let { commands = let step less more = if less then Decr else
+                                        if more then Incr else Idnt
+                   in  lift2 step pressLess pressMore
+      ; rand n = randomize 0 n commands
+      ; pos = lift2 (,) (rand 400) (rand 400)
+      ; color = lift3 rgb (rand 255) (rand 255) (rand 255)
+      ; mouse = lift2 (,) Mouse.isDown Mouse.position }
+  in  lift4 (,,,) commands pos color mouse
+
+controls = 
+  container 400 50 middle $
+  flow right [ butnLess, plainText "  Number of Squares   ",  butnMore ]
+
+display fs = collage 400 400 (outlined black (rect 400 400 (200,200)) : fs)
+
+main = lift (\forms -> display forms `above` controls)
+     $ run formsAutomaton allInput
